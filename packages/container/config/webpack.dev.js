6fea11c7c55786e7a9f5697756987e99019193c8
Gregory Mundy <gregmundy@gmail.com>
2020-11-25 15:26:35 -0500

    chore: add github action

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const commonConfig = require('./webpack.common');
@@ -19,11 +18,7 @@ const devConfig = {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
             shared: ['react', 'react-dom']
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
-    ]
+        }),    ]
 }
 
 module.exports = merge(commonConfig, devConfig);
