515e525a82644b73837d4fcfdc46b02562316aa6
Gregory Mundy <gregmundy@gmail.com>
2020-11-28 07:07:34 -0500

    feat: add navigation

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
     mode: 'development',
@@ -17,8 +18,9 @@ const devConfig = {
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
-            shared: ['react', 'react-dom']
-        }),    ]
+            shared: packageJson.dependencies,
+        })
+    ]
 }
 
 module.exports = merge(commonConfig, devConfig);
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import { createBrowserHistory } from 'history';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
@@ -8,15 +9,17 @@ const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 });
 
+const history = createBrowserHistory();
+
 export default () => {
     return (
-        <BrowserRouter>
+        <Router history={history}>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
                     <MarketingApp />
                 </div>
             </StylesProvider>
-        </BrowserRouter>
+        </Router>
     );
 }
